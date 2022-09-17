0e14237fe13f943c6a9a3a902ee6f3d4a9b91e57
lucas <lucaslcslcs1998@gmail.com>
2022-09-17 20:54:33 -0300

    vetores

---
+++ vetores/src/segundoExemplo.java
@@ -0,0 +1,15 @@
+public class segundoExemplo {
+    public static void main(String[] args) {
+        String[] letras = new String[5];
+        // [0] [1] [2] [3] [4]
+        letras[0] = "A";
+        letras[1] = "B";
+        letras[2] = "C";
+        letras[3] = "D";
+        letras[4] = "E";
+
+        for (int i = 0; i < letras.length; i++) {
+            System.out.println(letras[i]);
+        }
+    }
+}
