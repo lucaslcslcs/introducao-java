5d28cd2f8de17c2b97c533d035cfe58a7221421e
lucas <lucaslcslcs1998@gmail.com>
2022-09-17 20:01:30 -0300

    lacos numericos

---
+++ lacos-numericos/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1 2 3 4 5 6 7 8 9 10
+        // para uma variavel que inicia em 1, e vai ate 10, mudando 1-por-1, faça:
+        // calculos 1 x 1 A 10 x 10
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++){
+                System.out.println(j +"x"+ i + " = " + j * i);
+            }
+        }
+    }
+}
