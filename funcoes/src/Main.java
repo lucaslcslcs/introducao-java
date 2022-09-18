84eac7785a7a7b12d9933d56b23516f5b48c430f
lucas <lucaslcslcs1998@gmail.com>
2022-09-17 23:34:51 -0300

    funcoes

---
+++ funcoes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String nomeOriginal = "Let's Code";
+        saudacao(nomeOriginal);
+    }
+
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
+    }
+}
---
+++ funcoes/src/segundoExemplo.java
@@ -0,0 +1,8 @@
+public class segundoExemplo {
+    public static void main(String[] args) {
+        int resultado = soma(2, 3);
+    }
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
